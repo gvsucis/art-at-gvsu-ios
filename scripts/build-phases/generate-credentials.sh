6eb326fd7d23d7edf8395a435fa90e49e2a4a10f
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2023-10-15 16:36:57 -0500

    Build against iOS 17, bump deployment to 16

--- scripts/build-phases/generate-credentials.sh
+++ scripts/build-phases/generate-credentials.sh
@@ -2,11 +2,8 @@
 
 set -e
 
-# DERIVED_PATH=${BUILT_PRODUCTS_DIR}/../DerivedSources
-
 LOCAL_FIREBASE_PATH="${SRCROOT}/ArtAtGVSU/GoogleService-Info.plist"
 SAMPLE_FIREBASE_PATH="${SRCROOT}/GoogleService-Info-sample.plist"
-# FIREBASE_OUTPUT_PATH="${DERIVED_PATH}/GoogleService-Info.plist"
 
 if [ ! -f "$LOCAL_FIREBASE_PATH" ]; then
     echo "Copying mock GoogleService-Info.plist"
