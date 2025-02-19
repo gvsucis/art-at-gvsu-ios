2d832d8dc1fd42021f6148a3dd53f1e98321e5d6
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2025-02-18 18:59:39 -0600

    Update supported video extensions

--- ArtAtGVSU/URL+PathExtensionCheck.swift
+++ ArtAtGVSU/URL+PathExtensionCheck.swift
@@ -7,7 +7,10 @@
 
 import Foundation
 
-let ART_GALLERY_VIDEO_EXTENSIONS = ["m4v"]
+let ART_GALLERY_VIDEO_EXTENSIONS = [
+    "m4v",
+    "mp4"
+]
 
 extension URL {
     var hasVideoExtension: Bool {
