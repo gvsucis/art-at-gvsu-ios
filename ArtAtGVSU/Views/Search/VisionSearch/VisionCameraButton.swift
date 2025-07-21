251ac10efbc8c8f0c15fc7e0368bc168a5c3c405
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2025-07-21 10:14:09 -0500

    Add custom viewfinder for vision search (#62)

--- ArtAtGVSU/Views/Search/VisionSearch/VisionCameraButton.swift
+++ ArtAtGVSU/Views/Search/VisionSearch/VisionCameraButton.swift
@@ -7,7 +7,6 @@
 //
 
 import SwiftUI
-import UIKit
 
 struct VisionCameraButton: View {
   @State private var isPresentingCamera = false
@@ -29,7 +28,7 @@ struct VisionCameraButton: View {
           }) {
             Image(systemName: "camera")
           }.fullScreenCover(isPresented: $isPresentingCamera) {
-              CameraView(capturedImage: $capturedImage)
+              VisionSearchCamera(capturedImage: $capturedImage)
                   .onDisappear {
                       if capturedImage != nil {
                           showVisionResult = true
@@ -37,50 +36,5 @@ struct VisionCameraButton: View {
                   }
           }
       }
-  }
-}
-
-struct CameraView: UIViewControllerRepresentable {
-  @Binding var capturedImage: UIImage?
-
-  class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-    var parent: CameraView
-
-    init(parent: CameraView) {
-      self.parent = parent
-    }
-
-    func imagePickerController(
-      _ picker: UIImagePickerController,
-      didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]
-    ) {
-      if let image = info[.originalImage] as? UIImage {
-        parent.capturedImage = image
-      }
-      picker.dismiss(animated: true)
-    }
-
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-      picker.dismiss(animated: true)
-    }
-  }
-
-  func makeCoordinator() -> Coordinator {
-    Coordinator(parent: self)
-  }
-
-  func makeUIViewController(context: Context) -> UIImagePickerController {
-    let picker = UIImagePickerController()
-    picker.delegate = context.coordinator
-    picker.sourceType = .camera
-    return picker
-  }
-
-  func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
-}
-
-struct VisionCameraButton_Previews: PreviewProvider {
-  static var previews: some View {
-    VisionCameraButton()
   }
 }
