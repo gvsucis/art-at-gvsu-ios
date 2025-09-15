9d3e4ffa3c3abe99538bcfd1d3a088bfbc4502e3
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2025-09-15 14:39:54 -0500

    Fix image clipping on swipe

--- ArtAtGVSU/Views/Artworks/ArtworkDetailView.swift
+++ ArtAtGVSU/Views/Artworks/ArtworkDetailView.swift
@@ -28,7 +28,6 @@ struct ArtworkDetailView: View {
                                 )
                             }
                         }
-                        .scaledToFill()
                         .tag(representation.index)
                     }
                 }
